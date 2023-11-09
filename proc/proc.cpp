c564de13eb0377c38ac52ea8c9feff705a48cb19
Kemal Fadillah <fadillzzz@gmail.com>
2023-11-08 23:03:07 -0800

    couple of fixes

--- proc/proc.cpp
+++ proc/proc.cpp
@@ -1,54 +1,47 @@
-#include "../pch.h"
 #include "proc.h"
+#include "../pch.h"
 
-DWORD GetProcId(const wchar_t* procName)
-{
-	DWORD procId = 0;
-	HANDLE handleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+DWORD GetProcId(const wchar_t *procName) {
+    DWORD procId = 0;
+    HANDLE handleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-	if (handleSnapshot != INVALID_HANDLE_VALUE)
-	{
-		PROCESSENTRY32 procEntry;
-		procEntry.dwSize = sizeof(procEntry);
+    if (handleSnapshot != INVALID_HANDLE_VALUE) {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(procEntry);
 
-		while (Process32Next(handleSnapshot, &procEntry))
-		{
-			if (_wcsicmp(procEntry.szExeFile, procName) == 0)
-			{
-				procId = procEntry.th32ProcessID;
-				break;
-			}
-		}
-	}
+        while (Process32Next(handleSnapshot, &procEntry)) {
+            if (_wcsicmp((wchar_t *)procEntry.szExeFile, procName) == 0) {
+                procId = procEntry.th32ProcessID;
+                break;
+            }
+        }
+    }
 
-	CloseHandle(handleSnapshot);
+    CloseHandle(handleSnapshot);
 
-	return procId;
+    return procId;
 }
 
-uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* moduleName)
-{
-	uintptr_t address = 0;
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t *moduleName) {
+    uintptr_t address = 0;
 
-	HANDLE handleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+    HANDLE handleSnapshot = CreateToolhelp32Snapshot(
+        TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
 
-	if (handleSnapshot != INVALID_HANDLE_VALUE)
-	{
-		MODULEENTRY32 moduleEntry;
+    if (handleSnapshot != INVALID_HANDLE_VALUE) {
+        MODULEENTRY32 moduleEntry;
 
-		moduleEntry.dwSize = sizeof(moduleEntry);
+        moduleEntry.dwSize = sizeof(moduleEntry);
 
-		while (Module32Next(handleSnapshot, &moduleEntry))
-		{
-			if (_wcsicmp(moduleEntry.szModule, moduleName) == 0)
-			{
-				address = (uintptr_t)moduleEntry.modBaseAddr;
-				break;
-			}
-		}
-	}
+        while (Module32Next(handleSnapshot, &moduleEntry)) {
+            if (_wcsicmp((wchar_t *)moduleEntry.szModule, moduleName) == 0) {
+                address = (uintptr_t)moduleEntry.modBaseAddr;
+                break;
+            }
+        }
+    }
 
-	CloseHandle(handleSnapshot);
+    CloseHandle(handleSnapshot);
 
-	return address;
+    return address;
 }
